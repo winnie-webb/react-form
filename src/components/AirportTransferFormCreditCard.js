be82d8eb5dd909fd157aba841cac18b3d4499d27
winnie-webb <wbrown152020@gmail.com>
2021-01-07 13:29:34 -0800

    Removed CVC And Name On Card Input

--- src/components/AirportTransferFormCreditCard.js
+++ src/components/AirportTransferFormCreditCard.js
@@ -22,19 +22,10 @@ function AirportTransferFormCreditCard(){
 <h2>Checkout</h2>
 
       <div className="checkouts">
-          <div className="forms__form ">
-          <label htmlFor="name-on-card">Name On Card</label>
-          <input type="text" id="name-on-card" name="name-on-card" />
-          </div>
-  
+          
           <div className="forms__form">
           <label htmlFor="ccn">Card Number</label>
           <input id="ccn" name="ccn" type="tel" inputMode="numeric" pattern="[0-9\s]{13,19}" autoComplete="cc-number" maxLength="19" placeholder="0000 0000 0000 0000"/>
-          </div>
-  
-          <div className="forms__form">
-          <label htmlFor="cvc">CVC Code</label>
-          <input id="cvc" name="cvc" type="tel" />
           </div>
   
           <div className="forms__form">
